01026b3d1bd492ed92f61e8895fed3ebc8405c75
trecart <trecart@gmail.com>
2023-03-15 18:13:23 +0800

    169

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230315.180830';
+const cacheName = 'chirpy-20230315.181304';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
